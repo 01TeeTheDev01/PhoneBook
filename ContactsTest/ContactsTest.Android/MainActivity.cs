402a4b1685f7fd1ad28f8eaac9851521cb69b15a
Teethedev@outlook.com <Teethedev@outlook.com>
2022-06-17 20:03:03 +0200

    Changed app icon

--- ContactsTest/ContactsTest.Android/MainActivity.cs
+++ ContactsTest/ContactsTest.Android/MainActivity.cs
@@ -7,17 +7,20 @@ using Android.OS;
 
 namespace ContactsTest.Droid
 {
-    [Activity(Label = "Mailing List (Beta)", Icon = "@drawable/mail", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    [Activity(Label = "Phonebook Lite (Beta)", Icon = "@drawable/phonebook", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+
+    public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
+        /// <inheritdoc/>
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
-            global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
+            Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
+        /// <inheritdoc/>
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
